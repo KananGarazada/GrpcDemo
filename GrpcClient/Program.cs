6b980f7348a5f6730b04e2489630fa2db36026f7
Kanan Garazada <kanangarayev02@gmail.com>
2022-11-15 12:18:06 +0400

    add new grpc service

--- GrpcClient/Program.cs
+++ GrpcClient/Program.cs
@@ -1,3 +1,12 @@
-﻿
+﻿using Grpc.Net.Client;
+using GrpcDemo;
+
+//var input = new HelloRequest { Name= "Kanan" }; 
+//var channel = GrpcChannel.ForAddress("https://localhost:7125");
+//var client = new Greeter.GreeterClient(channel);
+
+//var reply = await client.SayHelloAsync(input);
+
+//Console.WriteLine(reply.Message);
 
 Console.ReadLine();
